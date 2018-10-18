9524b249ac9ad944711f417c834ec69e6aae85e5
Johnathon Wood <jaywood128@gmail.com>
2018-10-18 19:30:16 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
-new_array = comparison_array[array1, element1]
-  if array == new_array.include(array1,element1)
+
+  if array == array.include(element)
     return true
   else
     return false
